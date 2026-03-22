d084cc4e5a18abd03a3e5c1e3234c88849dc9934
Álefe Alexandre <contact.shokzin@gmail.com>
2026-03-22 18:53:42 -0300

    add: initial repository structure

---
+++ lua/curso01/variaveis.lua
@@ -0,0 +1,24 @@
+nome = "sk"
+num = 10
+valor = 3.14
+logica = true
+vazia = nil
+
+print("Tipo:",type(nome)," valor:",nome)
+print("Tipo:",type(num)," valor:",num)
+print(type(valor))
+print(type(logica))
+print(type(vazia))
+
+-- 
+
+soma = 10 + 5
+
+print("Soma:",soma)
+
+--
+num1 = 10
+num2 = 5
+subtracao = num1 - num2
+
+print("Subtracao:",subtracao)
